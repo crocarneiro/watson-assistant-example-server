79ff5570f341b9ad01a3b8fbc93f4b227326e978
Carlos Rafael de Oliveira Carneiro <contato@carlos.carneiro.nom.br>
2021-08-03 23:27:17 -0300

    First commit

---
+++ src/main/java/br/nom/carlos/carneiro/watsonassistantexampleserver/WatsonAssistantExampleServerApplication.java
@@ -0,0 +1,13 @@
+package br.nom.carlos.carneiro.watsonassistantexampleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatsonAssistantExampleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatsonAssistantExampleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/nom/carlos/carneiro/watsonassistantexampleserver/WatsonAssistantExampleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.nom.carlos.carneiro.watsonassistantexampleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatsonAssistantExampleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
